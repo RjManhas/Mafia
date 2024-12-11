86c0c9c137c3ca3a3d0b0c629e160f580b3c26f4
RjManhas <artaxnetworking@gmail.com>
2024-12-11 13:55:28 -0700

    a

--- backend/events/MessageEvents.js
+++ backend/events/MessageEvents.js
@@ -1,5 +1,9 @@
 module.exports = function (io, socket) {
     socket.on('message', (message) => {
-        io.in(socket.player.roomID).emit('message', `${socket.player.nickname}: ${message}`);
+        if (socket.player && socket.player.roomID) {
+            io.in(socket.player.roomID).emit('message', `${socket.player.nickname}: ${message}`);
+        } else {
+            console.error('Player is not defined or not in a room.');
+        }
     });
 };
